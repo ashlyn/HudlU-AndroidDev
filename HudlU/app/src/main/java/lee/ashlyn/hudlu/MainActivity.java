aba581faada6a01df2509d5d88d6646a61dd7b71
Ashlyn Lee <ashlynlee13@gmail.com>
2016-03-02 22:58:59 -0600

    HW 1 - change background color, text color, add log to settings menu

--- HudlU/app/src/main/java/lee/ashlyn/hudlu/MainActivity.java
+++ HudlU/app/src/main/java/lee/ashlyn/hudlu/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -44,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Log.d("HudlU", "Settings menu item clicked");
             return true;
         }
 
