7b8c3e25290776d9f76b09ec9bfeaa118b8a62d7
Ashlyn Lee <ashlynlee13@gmail.com>
2016-03-08 00:32:34 -0600

    Create Card View layout for items

--- HudlU/app/src/main/java/lee/ashlyn/hudlu/MyAdapter.java
+++ HudlU/app/src/main/java/lee/ashlyn/hudlu/MyAdapter.java
@@ -2,37 +2,46 @@ package lee.ashlyn.hudlu;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by ashlyn.lee on 3/8/16.
  */
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
-    
-    public MyAdapter(Context context, String[] data) {
 
+    private String[] mListData;
+
+    public MyAdapter(Context context, String[] data) {
+        mListData = data;
     }
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_view, parent, false);
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
     }
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-
+        holder.mTextView.setText(mListData[position]);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mListData.length;
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView mTextView;
 
         public MyViewHolder(View itemView) {
             super(itemView);
+            mTextView = (TextView) itemView.findViewById(R.id.list_text);
         }
     }
 }
